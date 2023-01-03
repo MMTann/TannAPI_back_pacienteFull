7dfc35b2e220c350663d9cd6c4482453e7b7ad93
mmtann <matheusmoura@tann.com.br>
2023-01-03 09:29:12 -0300

    feat: SSL certificate connections

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import app from "./index.js";
 import mongoose from "mongoose";
+import https from "https";
+import path from "path";
+import fs from "fs";
 import apiService from "./service/apiService.js";
 
 const db_user = process.env.db_user;
 const db_password = encodeURIComponent(process.env.db_password);
 const port = process.env.PORT || 3000;
 
+var options = {
+  key: fs.readFileSync("./src/cert/key.pem"),
+  cert: fs.readFileSync("./src/cert/cert.pem"),
+};
+
+const sslServer = https.createServer(options, app);
 // Connect to DB
 mongoose
   .connect(
@@ -13,7 +22,7 @@ mongoose
     { dbName: "DataLake" }
   )
   .then(() => {
-    app.listen(port, () => {
+    sslServer.listen(port, () => {
       console.log(
         `Connected to MongoDB! \nListening on: http://localhost:${port}`
       );
